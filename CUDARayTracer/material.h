38aeb85dbdf4e0c59698ca14f49db7358277f3da
Christian Blanton <cblanton@usc.edu>
2024-01-15 14:10:43 -0800

    Add project files.

---
+++ CUDARayTracer/material.h
@@ -0,0 +1,77 @@
+#ifndef MATERIALH
+#define MATERIALH
+
+struct hit_record;
+
+#include "ray.h"
+#include "hittable.h"
+
+class material {
+public:
+    __device__ virtual bool scatter(const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered, curandState* local_rand_state) const = 0;
+};
+
+class lambertian : public material {
+public:
+    __device__ lambertian(const vec3& a) : albedo(a) {}
+    __device__ virtual bool scatter(const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered, curandState* local_rand_state) const {
+        vec3 target = rec.p + rec.normal + random_in_unit_sphere(local_rand_state);
+        if (target.near_zero()) {
+            target = rec.normal;
+        }
+        scattered = ray(rec.p, target - rec.p);
+        attenuation = albedo;
+        return true;
+    }
+
+    vec3 albedo;
+};
+
+class metal : public material {
+public:
+    __device__ metal(const vec3& a, float f) : albedo(a) { if (f < 1) fuzz = f; else fuzz = 1; }
+    __device__ virtual bool scatter(const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered, curandState* local_rand_state) const {
+        vec3 reflected = reflect(unit_vector(r_in.direction()), rec.normal);
+        scattered = ray(rec.p, reflected + fuzz * random_in_unit_sphere(local_rand_state));
+        attenuation = albedo;
+        return (dot(scattered.direction(), rec.normal) > 0.0f);
+    }
+    vec3 albedo;
+    float fuzz;
+};
+
+class dielectric : public material {
+public:
+    __device__ dielectric(float index_of_refraction) : ir(index_of_refraction) {}
+    __device__ virtual bool scatter(const ray& r_in,
+        const hit_record& rec,
+        vec3& attenuation,
+        ray& scattered,
+        curandState* local_rand_state) const {
+        attenuation = vec3(1.0, 1.0, 1.0);
+        double refraction_ratio = rec.front_face ? (1.0 / ir) : ir;
+
+        vec3 unit_direction = unit_vector(r_in.direction());
+        double cos_theta = fmin(dot(-unit_direction, rec.normal), 1.0f);
+        double sin_theta = sqrt(1.0 - cos_theta * cos_theta);
+
+        bool cannot_refract = refraction_ratio * sin_theta > 1.0f;
+        vec3 direction;
+
+        if (cannot_refract || reflectance(cos_theta, refraction_ratio) > curand_uniform(local_rand_state))
+            direction = reflect(unit_direction, rec.normal);
+        else
+            direction = refract(unit_direction, rec.normal, refraction_ratio);
+
+        scattered = ray(rec.p, direction);
+        return true;
+    }
+    private:
+    __device__ float reflectance(float cosine, float ref_idx) const{
+            float r0 = (1.0f - ref_idx) / (1.0f + ref_idx);
+            r0 = r0 * r0;
+            return r0 + (1.0f - r0) * pow((1.0f - cosine), 5.0f);
+    }
+    float ir;
+};
+#endif
---
+++ CUDARayTracer/vec3.h
@@ -0,0 +1,178 @@
+#ifndef VEC3H
+#define VEC3H
+
+#include <math.h>
+#include <stdlib.h>
+#include <iostream>
+
+class vec3 {
+
+
+public:
+    __host__ __device__ vec3() {}
+    __host__ __device__ vec3(float e0, float e1, float e2) { e[0] = e0; e[1] = e1; e[2] = e2; }
+    __host__ __device__ inline float x() const { return e[0]; }
+    __host__ __device__ inline float y() const { return e[1]; }
+    __host__ __device__ inline float z() const { return e[2]; }
+    __host__ __device__ inline float r() const { return e[0]; }
+    __host__ __device__ inline float g() const { return e[1]; }
+    __host__ __device__ inline float b() const { return e[2]; }
+
+    __host__ __device__ inline const vec3& operator+() const { return *this; }
+    __host__ __device__ inline vec3 operator-() const { return vec3(-e[0], -e[1], -e[2]); }
+    __host__ __device__ inline float operator[](int i) const { return e[i]; }
+    __host__ __device__ inline float& operator[](int i) { return e[i]; };
+
+    __host__ __device__ inline vec3& operator+=(const vec3& v2);
+    __host__ __device__ inline vec3& operator-=(const vec3& v2);
+    __host__ __device__ inline vec3& operator*=(const vec3& v2);
+    __host__ __device__ inline vec3& operator/=(const vec3& v2);
+    __host__ __device__ inline vec3& operator*=(const float t);
+    __host__ __device__ inline vec3& operator/=(const float t);
+
+    __host__ __device__ inline float length() const { return sqrt(e[0] * e[0] + e[1] * e[1] + e[2] * e[2]); }
+    __host__ __device__ inline float squared_length() const { return e[0] * e[0] + e[1] * e[1] + e[2] * e[2]; }
+    __host__ __device__ bool near_zero() const {
+        // Return true if the vector is close to zero in all dimensions.
+        float s = 1e-8f;
+        return (fabs(e[0]) < s) && (fabs(e[1]) < s) && (fabs(e[2]) < s);
+    }
+    __host__ __device__ inline void make_unit_vector();
+
+
+    float e[3];
+};
+
+
+
+inline std::istream& operator>>(std::istream& is, vec3& t) {
+    is >> t.e[0] >> t.e[1] >> t.e[2];
+    return is;
+}
+
+inline std::ostream& operator<<(std::ostream& os, const vec3& t) {
+    os << t.e[0] << " " << t.e[1] << " " << t.e[2];
+    return os;
+}
+
+__host__ __device__ inline void vec3::make_unit_vector() {
+    float k = 1.0 / sqrt(e[0] * e[0] + e[1] * e[1] + e[2] * e[2]);
+    e[0] *= k; e[1] *= k; e[2] *= k;
+}
+
+__host__ __device__ inline vec3 operator+(const vec3& v1, const vec3& v2) {
+    return vec3(v1.e[0] + v2.e[0], v1.e[1] + v2.e[1], v1.e[2] + v2.e[2]);
+}
+
+__host__ __device__ inline vec3 operator-(const vec3& v1, const vec3& v2) {
+    return vec3(v1.e[0] - v2.e[0], v1.e[1] - v2.e[1], v1.e[2] - v2.e[2]);
+}
+
+__host__ __device__ inline vec3 operator*(const vec3& v1, const vec3& v2) {
+    return vec3(v1.e[0] * v2.e[0], v1.e[1] * v2.e[1], v1.e[2] * v2.e[2]);
+}
+
+__host__ __device__ inline vec3 operator/(const vec3& v1, const vec3& v2) {
+    return vec3(v1.e[0] / v2.e[0], v1.e[1] / v2.e[1], v1.e[2] / v2.e[2]);
+}
+
+__host__ __device__ inline vec3 operator*(float t, const vec3& v) {
+    return vec3(t * v.e[0], t * v.e[1], t * v.e[2]);
+}
+
+__host__ __device__ inline vec3 operator/(vec3 v, float t) {
+    return vec3(v.e[0] / t, v.e[1] / t, v.e[2] / t);
+}
+
+__host__ __device__ inline vec3 operator*(const vec3& v, float t) {
+    return vec3(t * v.e[0], t * v.e[1], t * v.e[2]);
+}
+
+__host__ __device__ inline float dot(const vec3& v1, const vec3& v2) {
+    return v1.e[0] * v2.e[0] + v1.e[1] * v2.e[1] + v1.e[2] * v2.e[2];
+}
+
+__host__ __device__ inline vec3 cross(const vec3& v1, const vec3& v2) {
+    return vec3((v1.e[1] * v2.e[2] - v1.e[2] * v2.e[1]),
+        (-(v1.e[0] * v2.e[2] - v1.e[2] * v2.e[0])),
+        (v1.e[0] * v2.e[1] - v1.e[1] * v2.e[0]));
+}
+
+
+__host__ __device__ inline vec3& vec3::operator+=(const vec3& v) {
+    e[0] += v.e[0];
+    e[1] += v.e[1];
+    e[2] += v.e[2];
+    return *this;
+}
+
+__host__ __device__ inline vec3& vec3::operator*=(const vec3& v) {
+    e[0] *= v.e[0];
+    e[1] *= v.e[1];
+    e[2] *= v.e[2];
+    return *this;
+}
+
+__host__ __device__ inline vec3& vec3::operator/=(const vec3& v) {
+    e[0] /= v.e[0];
+    e[1] /= v.e[1];
+    e[2] /= v.e[2];
+    return *this;
+}
+
+__host__ __device__ inline vec3& vec3::operator-=(const vec3& v) {
+    e[0] -= v.e[0];
+    e[1] -= v.e[1];
+    e[2] -= v.e[2];
+    return *this;
+}
+
+__host__ __device__ inline vec3& vec3::operator*=(const float t) {
+    e[0] *= t;
+    e[1] *= t;
+    e[2] *= t;
+    return *this;
+}
+
+__host__ __device__ inline vec3& vec3::operator/=(const float t) {
+    float k = 1.0 / t;
+
+    e[0] *= k;
+    e[1] *= k;
+    e[2] *= k;
+    return *this;
+}
+
+__host__ __device__ inline vec3 unit_vector(vec3 v) {
+    return v / v.length();
+}
+
+__device__ inline vec3 refract(const vec3& uv, const vec3& n, double etai_over_etat) {
+    auto cos_theta = fmin(dot(-uv, n), 1.0f);
+    vec3 r_out_perp = etai_over_etat * (uv + cos_theta * n);
+    vec3 r_out_parallel = -sqrt(fabs(1.0f - r_out_perp.squared_length())) * n;
+    return r_out_perp + r_out_parallel;
+}
+
+#define RANDVEC3 vec3(curand_uniform(local_rand_state),curand_uniform(local_rand_state),curand_uniform(local_rand_state))
+
+__device__ vec3 random_in_unit_sphere(curandState* local_rand_state) {
+    vec3 p;
+    do {
+        p = 2.0f * RANDVEC3 - vec3(1, 1, 1);
+    } while (p.squared_length() >= 1.0f);
+    return p;
+}
+
+__device__ vec3 reflect(const vec3& v, const vec3& n) {
+    return v - 2.0f * dot(v, n) * n;
+}
+
+__device__ vec3 random_in_unit_disk(curandState* local_rand_state) {
+    vec3 p;
+    do {
+        p = 2.0f * vec3(curand_uniform(local_rand_state), curand_uniform(local_rand_state), 0) - vec3(1, 1, 0);
+    } while (dot(p, p) >= 1.0f);
+    return p;
+}
+#endif
